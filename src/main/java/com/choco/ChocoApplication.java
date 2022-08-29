b57906e1cf33b8be4904c11a2758974bf83baf1d
btori0359 <btori0359@gmail.com>
2022-08-29 22:03:19 +0900

    Initial commit

---
+++ src/main/java/com/choco/ChocoApplication.java
@@ -0,0 +1,13 @@
+package com.choco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChocoApplication.class, args);
+    }
+
+}
